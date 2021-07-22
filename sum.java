c895ecd7e79efe6b27ef7df017d3581ee22ffd37
shivaniraul <shivani6999@gmail.com>
2021-07-22 13:14:15 +0530

    sec commit

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
 	public static void main(String args[]){
 		int num1=10,num2=20,sum;
 		sum = num1+num2;
-		System.out.println("Sum of these numbers :"+sum);
+		System.out.println("Sum of these numbers-modifing the content :"+sum);
 	}
 }
