2fffbd3b4a32ec4fd1a1034ea87241d9747ae919
shivaniraul <shivani6999@gmail.com>
2021-07-22 13:18:16 +0530

    commit after changes in diff

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class Diff{
+	public static void main(String args[]){
+		int num1=10,num2=20,sum;
+		diff = num1-num2;
+		System.out.println("Diff of these numbers :"+diff);
+	}
+}
+making changes after using git restore
